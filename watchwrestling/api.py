a96e93baabb73a295fbb9ab7f164f185ab8f1949
FancyJesse <jesus_andrade45@yahoo.com>
2020-12-27 23:53:57 -0800

    authentication changed to bearer

--- watchwrestling/api.py
+++ watchwrestling/api.py
@@ -5,8 +5,16 @@ import string
 
 import discord
 
-from .errors import IdleUserAPIError, ResourceNotFoundError, ValidationError
-
+from .errors import (
+    IdleUserAPIError,
+    BadRequest,
+    Unauthenticated,
+    InsufficientPrivileges,
+    ResourceNotFound,
+    MethodNotAllowed,
+    ConflictError,
+    ValidationError,
+)
 
 API_URL = "https://api.idleuser.com/"
 WEB_URL = "https://idleuser.com/projects/matches/"
@@ -18,25 +26,16 @@ class IdleUserAPI:
     def __init__(self, bot):
         self.bot = bot
 
-    async def get_response(self, route, params={}):
-        auth = await self.stored_auth_token()
-        params.update(auth)
-        return await self.get_idleusercom_response(route, params=params)
-
-    async def post_response(self, route, payload):
-        auth = await self.stored_auth_token()
-        return await self.post_idleusercom_response(route, params=auth, payload=payload)
-
-    async def get_idleusercom_response(self, route, params):
-        async with aiohttp.ClientSession() as session:
+    async def get_idleusercom_response(self, route, params={}):
+        headers = await self.get_headers()
+        async with aiohttp.ClientSession(headers=headers) as session:
             async with session.get(API_URL + route, params=params) as resp:
                 return await self.handle_response(resp)
 
-    async def post_idleusercom_response(self, route, params, payload):
-        async with aiohttp.ClientSession() as session:
-            async with session.post(
-                API_URL + route, params=params, json=payload
-            ) as resp:
+    async def post_idleusercom_response(self, route, payload={}):
+        headers = await self.get_headers()
+        async with aiohttp.ClientSession(headers=headers) as session:
+            async with session.post(API_URL + route, json=payload) as resp:
                 return await self.handle_response(resp)
 
     async def handle_response(self, response):
@@ -50,68 +49,94 @@ class IdleUserAPI:
             return data["data"]
         else:
             error_msg = data["error"]["description"]
-            if response.status == 404:
-                raise ResourceNotFoundError(error_msg)
+            if response.status == 400:
+                raise BadRequest(error_msg)
+            elif response.status == 401:
+                raise Unauthenticated(error_msg)
+            elif response.status == 403:
+                raise InsufficientPrivileges(error_msg)
+            elif response.status == 404:
+                raise ResourceNotFound(error_msg)
+            elif response.status == 405:
+                raise MethodNotAllowed(error_msg)
+            elif response.status == 409:
+                raise ConflictError(error_msg)
             elif response.status == 422:
                 raise ValidationError(error_msg)
             else:
                 log.error(data)
-                raise IdleUserAPIError(error_msg)
+                raise IdleUserAPIError("{} - {}".format(response.status, error_msg))
+
+    async def get_headers(self):
+        auth = await self.stored_auth_token()
+        auth_token = auth.get("auth_token", "")
+        headers = {"Authorization": "Bearer {}".format(auth_token)}
+        return headers
 
     async def stored_auth_token(self):
         auth = await self.bot.get_shared_api_tokens("idleuser")
         return auth
 
     async def get_user_by_id(self, user_id):
-        return await self.get_response("users/{}".format(user_id))
+        return await self.get_idleusercom_response(route="users/{}".format(user_id))
 
     async def get_user_by_username(self, username):
-        return await self.get_response("users/username/{}".format(username))
+        return await self.get_idleusercom_response(
+            route="users/username/{}".format(username)
+        )
 
     async def get_user_by_discord_id(self, discord_id):
-        return await self.get_response("users/discord/{}".format(discord_id))
+        return await self.get_idleusercom_response(
+            route="users/discord/{}".format(discord_id)
+        )
 
     async def get_user_stats_by_season_id(self, user_id, season_id):
-        return await self.get_response(
-            "watchwrestling/stats/user/{}/season/{}".format(user_id, season_id)
+        return await self.get_idleusercom_response(
+            route="watchwrestling/stats/user/{}/season/{}".format(user_id, season_id)
         )
 
     async def get_bet_by_id(self, match_id, user_id):
-        return await self.get_response(
-            "watchwrestling/bets/match/{}/user/{}".format(match_id, user_id)
+        return await self.get_idleusercom_response(
+            route="watchwrestling/bets/match/{}/user/{}".format(match_id, user_id)
         )
 
     async def get_leaderboard_by_season_id(self, season_id):
-        return await self.get_response(
-            "watchwrestling/stats/leaderboard/season/{}".format(season_id)
+        return await self.get_idleusercom_response(
+            route="watchwrestling/stats/leaderboard/season/{}".format(season_id)
         )
 
     async def get_match_by_id(self, match_id):
-        return await self.get_response(
-            "watchwrestling/matches/{}/detail".format(match_id)
+        return await self.get_idleusercom_response(
+            route="watchwrestling/matches/{}/detail".format(match_id)
         )
 
     async def get_openbet_matches(self):
-        return await self.get_response("watchwrestling/matches/betopen/detail")
+        return await self.get_idleusercom_response(
+            route="watchwrestling/matches/betopen/detail"
+        )
 
     async def get_current_match(self):
-        return await self.get_response("watchwrestling/matches/current/detail")
+        return await self.get_idleusercom_response(
+            route="watchwrestling/matches/current/detail"
+        )
 
     async def get_recent_match(self):
-        return await self.get_response("watchwrestling/matches/recent/detail")
+        return await self.get_idleusercom_response(
+            route="watchwrestling/matches/recent/detail"
+        )
 
     async def get_superstar_search(self, keyword):
-        return await self.get_response(
-            "watchwrestling/superstars/search/{}".format(keyword)
+        return await self.get_idleusercom_response(
+            route="watchwrestling/superstars/search/{}".format(keyword)
         )
 
     async def get_superstar_by_id(self, superstar_id):
-        return await self.get_response(
-            "watchwrestling/superstars/{}".format(superstar_id)
+        return await self.get_idleusercom_response(
+            route="watchwrestling/superstars/{}".format(superstar_id)
         )
 
     async def get_future_events(self):
-        return await self.get_response("watchwrestling/events/future")
+        return await self.get_idleusercom_response("watchwrestling/events/future")
 
     async def post_match_rating(self, user_id, match_id, rating):
         payload = {
@@ -119,7 +144,9 @@ class IdleUserAPI:
             "match_id": match_id,
             "rating": rating,
         }
-        return await self.post_response("watchwrestling/rate", payload)
+        return await self.post_idleusercom_response(
+            route="watchwrestling/rate", payload=payload
+        )
 
     async def post_match_bet(self, user_id, match_id, team_id, points):
         payload = {
@@ -128,13 +155,17 @@ class IdleUserAPI:
             "team": team_id,
             "points": points,
         }
-        return await self.post_response("watchwrestling/bet", payload)
+        return await self.post_idleusercom_response(
+            route="watchwrestling/bet", payload=payload
+        )
 
     async def post_user_login_token(self, user_id):
         payload = {
             "user_id": user_id,
         }
-        return await self.post_response("users/login/token", payload)
+        return await self.post_idleusercom_response(
+            route="users/login/token", payload=payload
+        )
 
     async def post_user_register(self, username, discord_id=None, chatango_id=None):
         payload = {
@@ -145,4 +176,6 @@ class IdleUserAPI:
             "discord_id": discord_id,
             "chatango_id": chatango_id,
         }
-        return await self.post_response("users/register", payload)
+        return await self.post_idleusercom_response(
+            route="users/register", payload=payload
+        )
